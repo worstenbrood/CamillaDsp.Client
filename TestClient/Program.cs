a40e68d168123455a7a4b988bf2f5144e3f2eb5e
Tim Horemans <tim.horemans@tenforce.com>
2026-01-20 13:19:57 +0100

    Updated test client

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -3,11 +3,13 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using CamillaDsp.Client;
 using CamillaDsp.Client.Models;
+
 public class Program
 {
     public static async Task Main(string[] args)
     {
         var client = new CamillaDspClient("ws://192.168.1.123:1234");
-        Debug.WriteLine($"Version: {await client.GetVersionAsync()}");
+        Console.WriteLine($"camilladsp version: {await client.GetVersionAsync()}");
+        Console.WriteLine($"volume: {await client.GetVolume()}dB");
     }
 }
