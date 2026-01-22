89b8415d6f759c4c5afac8fbce2113bdc9db3e8a
worstenbrood <worstenbrood@gmail.com>
2026-01-22 21:51:27 +0100

    Added DeviceFactory

---
+++ CamillaDsp.Client/Factories/DeviceFactory.cs
@@ -0,0 +1,19 @@
+﻿using CamillaDsp.Client.Models.Config.AudioDevices;
+
+namespace CamillaDsp.Client.Factories
+{
+    public static class DeviceFactory
+    {
+        public static AudioDevice CreateSignal(SignalTypes type, int frequency, float level) =>
+            new()
+            {
+                Type = DeviceTypes.SignalGenerator,
+                Signal = new()
+                {
+                    Type = type,
+                    Frequency = frequency,
+                    Level = level,
+                }
+            };
+    }
+}
