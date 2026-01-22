d06139630f9ec0ba47c83226529195243c411114
worstenbrood <worstenbrood@gmail.com>
2026-01-22 22:10:18 +0100

    Updated DeviceFactory

--- CamillaDsp.Client/Factories/DeviceFactory.cs
+++ CamillaDsp.Client/Factories/DeviceFactory.cs
@@ -15,5 +15,44 @@ namespace CamillaDsp.Client.Factories
                     Level = level,
                 }
             };
+
+        public static AudioDevice CreateRawFile(string filename, Formats format, int channels = 2,
+             int? extraSamples = null, int? skipBytes = null,int?  readBytes = null)
+        {
+            return new()
+            {
+                Type = DeviceTypes.RawFile,
+                Filename = filename,
+                Format = format,
+                Channels = channels,
+                ExtraSamples = extraSamples,
+                SkipBytes = skipBytes,
+                ReadBytes = readBytes
+            };
+        }
+
+        public static AudioDevice CreateFile(string filename, Formats format, int channels = 2,
+             bool? waveHeader = null)
+        {
+            return new()
+            {
+                Type = DeviceTypes.File,
+                Filename = filename,
+                Format = format,
+                Channels = channels,
+                WaveHeader = waveHeader
+            };
+        }
+
+        public static AudioDevice CreateWavFile(string filename, Formats? format = null, int channels = 2)
+        {
+            return new()
+            {
+                Type = DeviceTypes.WavFile,
+                Filename = filename,
+                Format = format,
+                Channels = channels
+            };
+        }
     }
 }
